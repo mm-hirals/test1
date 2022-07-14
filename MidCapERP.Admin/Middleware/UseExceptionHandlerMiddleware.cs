a603ad173060b1a890b3f832ae9c9ab7806947e6
Parshwa Kapadia <pkapadia@magnusminds.net>
2022-07-14 15:29:45 +0530

    Implement Error Email on Exception in Middleware

--- MidCapERP.Admin/Middleware/UseExceptionHandlerMiddleware.cs
+++ MidCapERP.Admin/Middleware/UseExceptionHandlerMiddleware.cs
@@ -1,6 +1,8 @@
 ﻿using MagnusMinds.Utility.EmailService;
+using MimeKit;
 using System.Net;
 using System.Text.Json;
+using UAParser;
 
 namespace MidCapERP.Admin.Middleware
 {
@@ -9,10 +11,12 @@ namespace MidCapERP.Admin.Middleware
         private readonly RequestDelegate _next;
         private ILogger<UseExceptionHandlerMiddleware> _logger;
         private IEmailSender _emailSender;
+        private readonly IConfiguration _configuration;
 
-        public UseExceptionHandlerMiddleware(RequestDelegate next)
+        public UseExceptionHandlerMiddleware(RequestDelegate next, IConfiguration configuration)
         {
             _next = next;
+            _configuration = configuration;
         }
 
         public async Task Invoke(HttpContext context)
@@ -41,20 +45,65 @@ namespace MidCapERP.Admin.Middleware
                         break;
                 }
 
-                var result = JsonSerializer.Serialize(new { message = error?.Message });
+                //UAParser
+                var userAgent = context.Request.Headers["User-Agent"];
+                var uaParser = Parser.GetDefault();
+                ClientInfo c = uaParser.Parse(userAgent);
+
+                // Use the information about the exception
+                var logEntry = new //OnlineScheduling.Entities.LogEntryEntity()
+                {
+                    RequestPath = context.Request.Host + context.Request.Path,
+                    TimeStamp = DateTime.Now,
+                    ActionDescriptor = context.Request?.RouteValues?.Values,
+                    IpAddress = Convert.ToString(context.Connection.RemoteIpAddress),
+                    Message = error.Message,
+                    Exception = Convert.ToString(error),
+                    Source = error.Source,
+                    StackTrace = error.StackTrace,
+                    Type = Convert.ToString(error.GetType()),
+                    BrowserName = Convert.ToString(c.UserAgent),
+                };
+
+                var result = JsonSerializer.Serialize(new { errorMessage = Newtonsoft.Json.JsonConvert.SerializeObject(logEntry) });
                 _logger.LogError(result);
-                //var message = new MimeMessage();
-                //message.To.Add(new MailboxAddress("Error Email", "kparmar@magnusminds.net"));
-                //message.Body = new TextPart("Plain") { Text = result };
-                //try
-                //{
-                //   await _emailSender.SendEmailAsync(message);
-                //}
-                //catch (Exception ex)
-                //{
-                //    throw ex;
-                //}
-                await response.WriteAsync(result);
+
+                var sendExceptionEmail = Convert.ToString(_configuration["AppSettings:SendExceptionEmail"]);
+                var exceptionEmailToList = Convert.ToString(_configuration["AppSettings:ExceptionEmailToList"]);
+
+                if (sendExceptionEmail == "1" && !string.IsNullOrEmpty(exceptionEmailToList))
+                {
+                    // Send log in email
+                    string htmlTableStart = "<table style=\"width: 100 %; border: 1px solid #e5e5e5;\" border=\"1\" cellspacing=\"0\" cellpadding=\"6\">";
+                    string htmlTrStart = "<tr>";
+                    string htmlTableEnd = "</table>";
+                    string htmlTrEnd = "</tr>";
+                    string htmlTdStart = "<td>";
+                    string htmlTdEnd = "</td>";
+                    string htmlContent = htmlTableStart + htmlTrStart + htmlTdStart + "RequestPath" + htmlTdEnd + htmlTdStart + logEntry.RequestPath + htmlTdEnd + htmlTrEnd
+                                         + htmlTrStart + htmlTdStart + "TimeStamp" + htmlTdEnd + htmlTdStart + logEntry.TimeStamp + htmlTdEnd + htmlTrEnd
+                                         + htmlTrStart + htmlTdStart + "ActionDescriptor" + htmlTdEnd + htmlTdStart + logEntry.ActionDescriptor + htmlTdEnd + htmlTrEnd
+                                         + htmlTrStart + htmlTdStart + "IpAddress" + htmlTdEnd + htmlTdStart + logEntry.IpAddress + htmlTdEnd + htmlTrEnd
+                                         + htmlTrStart + htmlTdStart + "Message" + htmlTdEnd + htmlTdStart + logEntry.Message.Replace("\r\n", Environment.NewLine) + htmlTdEnd + htmlTrEnd
+                                         + htmlTrStart + htmlTdStart + "Exception" + htmlTdEnd + htmlTdStart + logEntry.Exception.Replace("\r\n", Environment.NewLine) + htmlTdEnd + htmlTrEnd
+                                         + htmlTrStart + htmlTdStart + "Source" + htmlTdEnd + htmlTdStart + logEntry.Source + htmlTdEnd + htmlTrEnd
+                                         + htmlTrStart + htmlTdStart + "StackTrace" + htmlTdEnd + htmlTdStart + logEntry.StackTrace.Replace("\r\n", Environment.NewLine) + htmlTdEnd + htmlTrEnd
+                                         + htmlTrStart + htmlTdStart + "Type" + htmlTdEnd + htmlTdStart + logEntry.Type + htmlTdEnd + htmlTrEnd
+                                         + htmlTrStart + htmlTdStart + "BrowserName" + htmlTdEnd + htmlTdStart + logEntry.BrowserName + htmlTdEnd + htmlTrEnd
+                                         + htmlTableEnd;
+
+                    if (logEntry.Message != "The client has disconnected")
+                    {
+                        var message = new MimeMessage();
+                        var toEmailList = exceptionEmailToList.Split(',');
+                        foreach (var item in toEmailList)
+                            message.To.Add(new MailboxAddress("MidCap-ERP Error Email", item));
+                        message.Subject = "Error Exception | MidCap-ERP | " + DateTime.Now;
+                        message.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = htmlContent };
+                        _emailSender.SendEmailAsync(message);
+                        //await Task.Run(() => { _emailSender.SendEmailAsync(message); });
+                    }
+                }
             }
         }
     }
