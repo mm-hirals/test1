bbae84ef598a384d2c0328e3fe7f8978c15212df
Dhruvi Patel <mm.dhruvi@gmail.com>
2022-09-16 17:00:28 +0530

    ERP-26_Product :: Added code for ProductImage Create and Update

--- MidCapERP.BusinessLogic/Interface/IProductBL.cs
+++ MidCapERP.BusinessLogic/Interface/IProductBL.cs
@@ -1,5 +1,6 @@
 ﻿using MidCapERP.Dto.DataGrid;
 using MidCapERP.Dto.Product;
+using MidCapERP.Dto.ProductImage;
 using MidCapERP.Dto.ProductMaterial;
 
 namespace MidCapERP.BusinessLogic.Interface
@@ -12,6 +13,8 @@ namespace MidCapERP.BusinessLogic.Interface
 
         public Task<ProductRequestDto> GetById(Int64 Id, CancellationToken cancellationToken);
 
+        public Task<List<ProductImageRequestDto>> GetImageByProductId(long Id, CancellationToken cancellationToken);
+
         public Task<List<ProductMaterialRequestDto>> GetMaterialByProductId(Int64 Id, CancellationToken cancellationToken);
 
         public Task<ProductRequestDto> CreateProduct(ProductRequestDto model, CancellationToken cancellationToken);
@@ -21,5 +24,7 @@ namespace MidCapERP.BusinessLogic.Interface
         public Task<ProductRequestDto> UpdateProduct(int Id, ProductRequestDto model, CancellationToken cancellationToken);
 
         public Task<ProductRequestDto> UpdateProductDetail(int Id, ProductRequestDto model, CancellationToken cancellationToken);
+
+        public Task<ProductImageRequestDto> SaveImages(long productId, ProductMainRequestDto model, CancellationToken cancellationToken);
     }
 }
