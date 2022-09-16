139ff23f7042c170a61710c2d6e85e9fce6cf383
Rutul Patel <mm.rutulp@gmail.com>
2022-09-16 16:54:54 +0530

    ERP-26 :: Added Material Related changes

--- MidCapERP.BusinessLogic/Interface/IProductBL.cs
+++ MidCapERP.BusinessLogic/Interface/IProductBL.cs
@@ -21,5 +21,8 @@ namespace MidCapERP.BusinessLogic.Interface
         public Task<ProductRequestDto> UpdateProduct(int Id, ProductRequestDto model, CancellationToken cancellationToken);
 
         public Task<ProductRequestDto> UpdateProductDetail(int Id, ProductRequestDto model, CancellationToken cancellationToken);
+
+        public Task<List<ProductMaterialRequestDto>> CreateProductMaterial(int productId, List<ProductMaterialRequestDto> productMaterialRequestList, CancellationToken cancellationToken);
+        public Task<ProductRequestDto?> UpdateProductCost(int Id, ProductMainRequestDto model, CancellationToken cancellationToken);
     }
 }
