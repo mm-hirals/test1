6bb75d5bc2577469a0811a19ffef3b6d5e4eda45
Kishan Kalena <mm.kishankalena@gmail.com>
2022-09-19 15:40:30 +0530

    ERP-API-30 :: ADDED Order Dto

---
+++ MidCapERP.Dto/Order/OrderResponseDto.cs
@@ -0,0 +1,28 @@
+﻿namespace MidCapERP.Dto.Frames
+{
+    public class OrderResponseDto
+    {
+        public Int64 OrderId { get; set; }
+
+        public string OrderNo { get; set; }
+        public Int64 CustomerID { get; set; }
+        public decimal GrossTotal { get; set; }
+        public decimal Discount { get; set; }
+        public decimal ReferralDiscount { get; set; }
+        public decimal? TotalAmount { get; set; }
+        public decimal GSTTaxAmount { get; set; }
+        public decimal PayableAmount { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public string Comments { get; set; }
+        public string GSTNo { get; set; }
+        public int Status { get; set; }
+        public bool IsDraft { get; set; }
+        public bool IsDeleted { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime CreatedUTCDate { get; set; }
+        public int? UpdatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public DateTime? UpdatedUTCDate { get; set; }
+    }
+}
