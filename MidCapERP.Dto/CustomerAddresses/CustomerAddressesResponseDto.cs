fde0802e02c6a51668a72d75788bf3684ec771d9
Ravi Lavadiya <mm.ravilavadiya@gmail.com>
2022-09-20 11:05:11 +0530

    ERP-22(Bugfixing) :: Caption related changes in application

--- MidCapERP.Dto/CustomerAddresses/CustomerAddressesResponseDto.cs
+++ MidCapERP.Dto/CustomerAddresses/CustomerAddressesResponseDto.cs
@@ -28,7 +28,7 @@ namespace MidCapERP.Dto.CustomerAddresses
         [DisplayName("State")]
         public string State { get; set; }
 
-        [DisplayName("ZipCode")]
+        [DisplayName("PinCode")]
         public string ZipCode { get; set; }
 
         [DisplayName("Default Address")]
