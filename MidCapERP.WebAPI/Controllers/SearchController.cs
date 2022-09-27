806eb96b4b4e34e0983058091cba2bec707d1bf1
Kishan Kalena <mm.kishankalena@gmail.com>
2022-09-27 18:55:05 +0530

    ERP-API-26 :: Product Related Changes

--- MidCapERP.WebAPI/Controllers/SearchController.cs
+++ MidCapERP.WebAPI/Controllers/SearchController.cs
@@ -38,21 +38,12 @@ namespace MidCapERP.WebAPI.Controllers
             else
                 return new ApiResponse(message: "No Data Found", result: productData, statusCode: 404);
         }
-            List<ProductForDorpDownByModuleNoResponseDto> productData = new List<ProductForDorpDownByModuleNoResponseDto>();
-            productData.AddRange(await _unitOfWorkBL.ProductBL.GetProductForDropDownByModuleNo(modelNo, cancellationToken));
-            productData.AddRange(await _unitOfWorkBL.FabricBL.GetFabricForDropDownByModuleNo(modelNo, cancellationToken));
-            productData.AddRange(await _unitOfWorkBL.PolishBL.GetPolishForDropDownByModuleNo(modelNo, cancellationToken));
-            if (productData != null && productData.Count > 0)
-                return new ApiResponse(message: "Data Found", result: productData, statusCode: 200);
-            else
-                return new ApiResponse(message: "No Data Found", result: productData, statusCode: 404);
-        }
-
-        [HttpGet("/Search/{modelNo}/{productType}")]
+           
+        [HttpGet("{modelNo}/{productType}")]
         [Authorize(ApplicationIdentityConstants.Permissions.Product.View)]
         public async Task<ApiResponse> GetDetails(string modelNo, string productType, CancellationToken cancellationToken)
         {
-            if (productType == "Product")
+            if (productType.ToLower() == "product")
             {
                 IList<ProductForDetailsByModuleNoResponceDto> productData = new List<ProductForDetailsByModuleNoResponceDto>();
                 productData = await _unitOfWorkBL.ProductBL.GetProductForDetailsByModuleNo(modelNo, cancellationToken);
@@ -60,14 +51,14 @@ namespace MidCapERP.WebAPI.Controllers
                     return new ApiResponse(message: "No Data Found", result: productData, statusCode: 404);
                 return new ApiResponse(message: "Data Found", result: productData, statusCode: 200);
             }
-            else if (productType == "Fabric")
+            else if (productType.ToLower() == "fabric")
             {
                 var frabricData = await _unitOfWorkBL.FabricBL.GetFabricForDetailsByModuleNo(modelNo, cancellationToken);
                 if (frabricData == null)
                     return new ApiResponse(message: "No Data Found", result: frabricData, statusCode: 200);
                 return new ApiResponse(message: "Data Found", result: frabricData, statusCode: 200);
             }
-            else if (productType == "Polish")
+            else if (productType.ToLower() == "polish")
             {
                 var polishData = await _unitOfWorkBL.PolishBL.GetPolishForDetailsByModuleNo(modelNo, cancellationToken);
                 if (polishData == null)
