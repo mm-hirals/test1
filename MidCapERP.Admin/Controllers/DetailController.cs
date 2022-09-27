c8bbd72f5bed4588a70e09b2e02e61d701ca1a6a
Parshwa Kapadia <pkapadia@magnusminds.net>
2022-09-27 09:55:44 +0530

    Remove Product Detail page and Add new Detail controller for Product Detail and Order Detail page access anonymously

---
+++ MidCapERP.Admin/Controllers/DetailController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using MidCapERP.BusinessLogic.UnitOfWork;
+
+namespace MidCapERP.Admin.Controllers
+{
+    public class DetailController : Controller
+    {
+        private readonly IUnitOfWorkBL _unitOfWorkBL;
+
+        public DetailController(IUnitOfWorkBL unitOfWorkBL)
+        {
+            _unitOfWorkBL = unitOfWorkBL;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("/ProductDetail/{id}")]
+        public async Task<IActionResult> ProductDetail(int Id, CancellationToken cancellationToken)
+        {
+            var data = await _unitOfWorkBL.ProductBL.GetById(Id, cancellationToken);
+            return View("ProductDetail", data);
+        }
+    }
+}
