fba58cac06aeedb0284f69b6b2c292786fcc402d
Dhruvi Patel <mm.dhruvi@gmail.com>
2022-10-10 19:41:32 +0530

    Added price breakup

---
+++ MidCapERP.Admin/wwwroot/js/OrderMain.js
@@ -0,0 +1,23 @@
+﻿//$(document).ready(function () {
+//    $("#divOrderInfo").load('/Order/CreateProductBasicDetail' + "?ProductId=" + $("#hdnProductId").val());
+//    $("#divOrderSetDetailPartial").load();
+
+//    //if ($("#hdnOrderId").val() > 0) {
+//    //    $("#divOrderSetDetailPartial").load('/Order/CreateProductDetail' + "?ProductId=" + $("#hdnProductId").val());
+//    //    $("#divProductImagePartial").load('/Product/CreateProductImage' + "?ProductId=" + $("#hdnProductId").val());
+//    //    $("#divProductMaterialPartial").load('/Product/CreateProductMaterial' + "?ProductId=" + $("#hdnProductId").val());
+//    //    $("#divProductActivityPartial").load('/Product/GetProductActivity' + "?ProductId=" + $("#hdnProductId").val());
+//    //    $("#divProductWorkflowPartial").load('/Product/CreateProductWorkFlow' + "?ProductId=" + document.getElementById("hdnProductId").value);
+//    //}
+//});
+
+//$(document).on("shown.bs.tab", 'button[data-bs-toggle="tab"]', function (e) {
+//    var tabId = $(e.target).attr("id")
+//    if (tabId == "nav-images-tab") {
+//        $("#divOrderImagePartial").load();
+//        //$("#divOrderImagePartial").load('/Order/CreateProductImage' + "?ProductId=" + $("#hdnProductId").val());
+//    } else if (tabId == "nav-rowmaterial-tab") {
+//        $("#divOrderActivityPartial").load();
+//        //$("#divOrderActivityPartial").load('/Order/CreateProductMaterial' + "?ProductId=" + $("#hdnProductId").val());
+//    }
+//});
