4413b5607b4c421e2e02e179bc99f4693dab4388
Rutul Patel <mm.rutulp@gmail.com>
2022-10-10 13:36:31 +0530

    Order_Page_Design related Changes

--- MidCapERP.Admin/wwwroot/js/Product.js
+++ MidCapERP.Admin/wwwroot/js/Product.js
@@ -7,10 +7,24 @@ $(document).ready(function () {
 
     if ($("#hdnProductId").val() > 0) {
         $("#divProductDetailPartial").load('/Product/CreateProductDetail' + "?ProductId=" + $("#hdnProductId").val());
-        $("#divProductImagePartial").load('/Product/CreateProductImage' + "?ProductId=" + $("#hdnProductId").val());
-        $("#divProductMaterialPartial").load('/Product/CreateProductMaterial' + "?ProductId=" + $("#hdnProductId").val());
-        $("#divProductActivityPartial").load('/Product/GetProductActivity' + "?ProductId=" + $("#hdnProductId").val());
+        //$("#divProductImagePartial").load('/Product/CreateProductImage' + "?ProductId=" + $("#hdnProductId").val());
+        //$("#divProductMaterialPartial").load('/Product/CreateProductMaterial' + "?ProductId=" + $("#hdnProductId").val());
+        //$("#divProductActivityPartial").load('/Product/GetProductActivity' + "?ProductId=" + $("#hdnProductId").val());
         //$("#divProductWorkflowPartial").load('/Product/CreateProductWorkFlow' + "?ProductId=" + document.getElementById("hdnProductId").value);
+    }
+});
+
+$(document).on("shown.bs.tab", 'button[data-bs-toggle="tab"]', function (e) {
+    debugger;
+    var tabId = $(e.target).attr("id")
+    if (tabId == "nav-images-tab") {
+        $("#divProductImagePartial").load('/Product/CreateProductImage' + "?ProductId=" + $("#hdnProductId").val());
+    } else if (tabId == "nav-rowmaterial-tab") {
+        $("#divProductMaterialPartial").load('/Product/CreateProductMaterial' + "?ProductId=" + $("#hdnProductId").val());
+    } else if (tabId == "nav-detail-tab") {
+        $("#divProductDetailPartial").load('/Product/CreateProductDetail' + "?ProductId=" + $("#hdnProductId").val());
+    } else if (tabId == "nav-productActivity") {
+        $("#divProductActivityPartial").load('/Product/GetProductActivity' + "?ProductId=" + $("#hdnProductId").val());
     }
 });
 
