71213a08b86a2239b8cbab85060cca0fa9d83d51
Kishan Kalena <mm.kishankalena@gmail.com>
2022-10-10 11:00:21 +0530

    ERP-5-Validation :: Bug Fix Change Validation Related Changes Validation Property Remove In ProductMaterialRequestDto

--- MidCapERP.Dto/ProductMaterial/ProductMaterialRequestDto.cs
+++ MidCapERP.Dto/ProductMaterial/ProductMaterialRequestDto.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace MidCapERP.Dto.ProductMaterial
+﻿namespace MidCapERP.Dto.ProductMaterial
 {
     public class ProductMaterialRequestDto
     {
@@ -8,12 +6,7 @@ namespace MidCapERP.Dto.ProductMaterial
         public long ProductId { get; set; }
         public int SubjectTypeId { get; set; }
         public int SubjectId { get; set; }
-
-        [Required]
-        [RegularExpression("/^\\d+$/", ErrorMessage = "The Quentite not Valid")]
-        [Range(0, 999)]
         public int Qty { get; set; }
-
         public decimal CostPrice { get; set; }
         public string? UnitType { get; set; }
         public int CreatedBy { get; set; }
