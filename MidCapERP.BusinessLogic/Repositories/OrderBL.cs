5c6558b16b8568a41fe65ca3a10ba2f2cd8be993
Parshwa Kapadia <pkapadia@magnusminds.net>
2022-10-13 13:40:58 +0530

    Order: Update calculation for Discount Percentage by Product

--- MidCapERP.BusinessLogic/Repositories/OrderBL.cs
+++ MidCapERP.BusinessLogic/Repositories/OrderBL.cs
@@ -300,10 +300,13 @@ namespace MidCapERP.BusinessLogic.Repositories
         {
             //Cost Calculation
             Random generator = new Random();
-            model.OrderNo = await _unitOfWorkDA.OrderDA.CreateOrderNo("R", cancellationToken); ;
-            //model.OrderNo = Convert.ToString(DateTime.Now.Year) + "-" + "R" + generator.Next(1, 99999).ToString("D5");
+            model.OrderNo = await _unitOfWorkDA.OrderDA.CreateOrderNo("R", cancellationToken);
             model.GrossTotal = model.OrderSetRequestDto.Sum(x => x.OrderSetItemRequestDto.Sum(x => x.UnitPrice * x.Quantity));
-            model.Discount = model.OrderSetRequestDto.Sum(x => x.OrderSetItemRequestDto.Sum(x => x.DiscountPrice));
+            decimal discountAmount = 0.00m;
+            foreach (var item in model.OrderSetRequestDto)
+                foreach (var item2 in item.OrderSetItemRequestDto)
+                    discountAmount += ((item2.UnitPrice * item2.Quantity) * item2.DiscountPrice / 100);
+            model.Discount = Math.Round(Math.Round(discountAmount), 2);
             model.TotalAmount = model.GrossTotal - model.Discount;
             model.GSTTaxAmount = Math.Round(Math.Round((model.TotalAmount * 18) / 100), 2);
 
@@ -337,9 +340,13 @@ namespace MidCapERP.BusinessLogic.Repositories
         {
             var oldData = await OrderGetById(Id, cancellationToken);
             oldData.GrossTotal = model.OrderSetRequestDto.Sum(x => x.OrderSetItemRequestDto.Sum(x => x.UnitPrice * x.Quantity));
-            oldData.Discount = model.OrderSetRequestDto.Sum(x => x.OrderSetItemRequestDto.Sum(x => x.DiscountPrice));
-            oldData.TotalAmount = model.GrossTotal - model.Discount;
-            oldData.GSTTaxAmount = Math.Round(Math.Round((model.TotalAmount * 18) / 100), 2);
+            decimal discountAmount = 0.00m;
+            foreach (var item in model.OrderSetRequestDto)
+                foreach (var item2 in item.OrderSetItemRequestDto)
+                    discountAmount += Math.Round(Math.Round(((item2.UnitPrice * item2.Quantity) * item2.DiscountPrice / 100)), 2);
+            oldData.Discount = Math.Round(Math.Round(discountAmount), 2);
+            oldData.TotalAmount = oldData.GrossTotal - oldData.Discount;
+            oldData.GSTTaxAmount = Math.Round(Math.Round((oldData.TotalAmount * 18) / 100), 2);
             oldData.UpdatedBy = _currentUser.UserId;
             oldData.UpdatedDate = DateTime.Now;
             oldData.UpdatedUTCDate = DateTime.UtcNow;
@@ -365,7 +372,8 @@ namespace MidCapERP.BusinessLogic.Repositories
                         oldOrderSetItem.Quantity = orderSetItem.Quantity;
                         oldOrderSetItem.UnitPrice = orderSetItem.UnitPrice;
                         oldOrderSetItem.DiscountPrice = orderSetItem.DiscountPrice;
-                        oldOrderSetItem.TotalAmount = (orderSetItem.UnitPrice * orderSetItem.Quantity) - orderSetItem.DiscountPrice;
+                        var discountAmountP = Math.Round(Math.Round(((orderSetItem.UnitPrice * orderSetItem.Quantity) * orderSetItem.DiscountPrice / 100)), 2);
+                        oldOrderSetItem.TotalAmount = (orderSetItem.UnitPrice * orderSetItem.Quantity) - discountAmountP;
                         oldOrderSetItem.Comment = orderSetItem.Comment;
                         oldOrderSetItem.UpdatedBy = _currentUser.UserId;
                         oldOrderSetItem.UpdatedDate = DateTime.Now;
@@ -631,7 +639,8 @@ namespace MidCapERP.BusinessLogic.Repositories
             orderSetItem.Quantity = orderSetItemRequestDto.Quantity;
             orderSetItem.UnitPrice = orderSetItemRequestDto.UnitPrice;
             orderSetItem.DiscountPrice = orderSetItemRequestDto.DiscountPrice;
-            orderSetItem.TotalAmount = (orderSetItemRequestDto.UnitPrice * orderSetItemRequestDto.Quantity) - orderSetItemRequestDto.DiscountPrice;
+            var discountAmount = Math.Round(Math.Round(((orderSetItemRequestDto.UnitPrice * orderSetItemRequestDto.Quantity) * orderSetItemRequestDto.DiscountPrice / 100)), 2);
+            orderSetItem.TotalAmount = (orderSetItemRequestDto.UnitPrice * orderSetItemRequestDto.Quantity) - discountAmount;
             orderSetItem.Comment = orderSetItemRequestDto.Comment;
             orderSetItem.MakingStatus = (int)ProductStatusEnum.Pending;
             orderSetItem.CreatedBy = _currentUser.UserId;
